2df930ef385fea202f0de747a4081f63f5abea3d
GTS-0522 <jobsuke.cap@gmail.com>
2026-07-12 08:24:39 +0900

    Update config.js

--- student-site/config.js
+++ student-site/config.js
@@ -1,5 +1,5 @@
 window.MENDAN_CONFIG = {
-  GAS_WEB_APP_URL: "ここにGASのウェブアプリURLを貼り付け",
+  GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxu41BUiHSxQXPVILsZ2xy0BwRXNqGzfor69q7r9WKWTR-otLsI0uJ4-WHJvzyavEVDQQ/exec",
   REFRESH_INTERVAL_MS: 30000
 };
 
