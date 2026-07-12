aba9551451bbde0e81d79bb9ed3bc81250690acc
GTS-0522 <jobsuke.cap@gmail.com>
2026-07-12 09:01:41 +0900

    Delete admin-teacher-R8 directory

--- admin-teacher-R8/config.js
+++
@@ -1,5 +0,0 @@
-window.MENDAN_CONFIG = {
-  GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxu41BUiHSxQXPVILsZ2xy0BwRXNqGzfor69q7r9WKWTR-otLsI0uJ4-WHJvzyavEVDQQ/exec",
-  REFRESH_INTERVAL_MS: 20000
-};
-
